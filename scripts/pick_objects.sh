07a0be42f495f522829b4f2f0fde24c5a3f94f39
LucasIvankio <ivankiolucas@gmail.com>
2023-04-07 00:25:15 -0700

    First commit on the Home Service Robot project

---
+++ scripts/pick_objects.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/robond/Desktop/ROS/Project5/catkin_ws/src/turtlebot_simulator/turtlebot_gazebo/worlds/house.world" &
+sleep 5
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/robond/Desktop/ROS/Project5/catkin_ws/src/turtlebot_simulator/turtlebot_gazebo/maps/map.yaml" &
+sleep 5
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 10
+xterm -e " roslaunch pick_objects pick_objects.launch" &
